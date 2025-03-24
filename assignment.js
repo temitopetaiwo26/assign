8b84bf398f57f6c8618e5f03f334a6e6c2bba38e
temitopetaiwo26 <taiwotemitope26@yahoo.com>
2025-03-24 15:44:15 -0800

    my javascript excersis

---
+++ assignment.js
@@ -0,0 +1,92 @@
+// let country = "ghana"
+// let nation = "africa"
+// population = "200000"
+// console.log(country);
+// console.log(nation);
+// console.log(population);
+
+
+// let isIsIand = false;
+// let language = 
+
+// console.log(typeof isIsIand);
+// console.log(typeof population);
+// console.log(typeof country);
+// console.log(typeof language);
+
+// values comes in either OBJECT OR PREMITIVE
+
+
+// */Set the value of language to the language spoken
+//  where you live (some countries have multiple languages, 
+// but just choose one).
+
+// Think about which variables should be const variables 
+// (which values will never change, and which might change?).
+//  Then, change these variables to const.
+
+// Try to change one of the changed variables now,
+//  and observe what happens.
+
+// language = 'english'
+// const country = 'ghana'
+// const continent = 'africa'
+// const isIsIand = 'false'
+// isIsIand = true;
+
+
+//If your country split in half, 
+// and each half would contain half the population, 
+// then how many people would live in each half?
+
+//Increase the population of your country by 1 and log the result to the console.
+
+//Finland has a population of 6 million.
+ //Does your country have more people than Finland?
+
+//The average population of a country is 33 million people.
+ //Does you country have less people than the average country?
+
+// Based on the variables you created,
+//  create a new variable description which 
+//  contains a string with this format: 
+//  'Portugal is in Europe, and its 11 million people speak portuguese'.
+
+
+// let contryPopulation = 100000;
+// let halfPopulation = 100000 / 2;
+// console.log(halfPopulation);
+
+// let contryPopulation =100000;
+// contryPopulation++;
+// console.log(contryPopulation);
+
+// let finlandPopulation = 6000000;
+// let mycountryPopulation = 100000;
+
+// if(mycountryPopulation > finlandPopulation){
+
+// console.log("my country is more ppopulated");
+// }else{
+
+// console.log("finland is more populated");
+// }
+
+let mycountryPopulation = 100000;
+let avrragePopulation = 33000000;
+ 
+if ( mycountryPopulation < avrragePopulation){
+    console.log("my country has lesser population");
+
+}else{
+    console.log("my country has more population than arrave ");
+}
+
+let country = "portugal";
+let continent = "Europe"
+let population = 11;
+let language = "portuguese";
+
+let description = `portugal is in Europe and its 11 million people speaking portugal`;
+console.log(description);
+
